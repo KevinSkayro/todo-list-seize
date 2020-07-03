617816c2a8f8b267370be3127080289cb0d54fe3
Kevin Skayro <kev1n-1997@outlook.com>
2020-07-03 09:43:21 -0400

    figured out a way to change the containers with the buttons on the sidebar, also added temporary background colors to see it work, it will be deleted in the future

--- code.js
+++ code.js
@@ -2,8 +2,33 @@
 const todoInput = document.querySelector(".todo-input");
 const todoInputBtn = document.querySelector(".btn-input");
 const todoCanvas = document.querySelector(".todo-list");
+const todoDays = document.querySelectorAll(".week");
+const todoDaysCanvas = document.querySelectorAll(".todo-canva");
+//days of the week selectors
+const sunday = document.querySelector(".sunday");
+const monday = document.querySelector(".monday");
+const tuesday = document.querySelector(".tuesday");
+const wednesday = document.querySelector(".wednesday");
+const thursday = document.querySelector(".thursday");
+const friday = document.querySelector(".friday");
+const saturday = document.querySelector(".saturday");
+const day1 = document.querySelector(".day1");
+const day2 = document.querySelector(".day2");
+const day3 = document.querySelector(".day3");
+const day4 = document.querySelector(".day4");
+const day5 = document.querySelector(".day5");
+const day6 = document.querySelector(".day6");
+const day7 = document.querySelector(".day7");
+
 //Event listeners
 todoInputBtn.addEventListener("click", addTodoItem);
+sunday.addEventListener("click", openSunday);
+monday.addEventListener("click", openMonday);
+tuesday.addEventListener("click", openTuesday);
+wednesday.addEventListener("click", openWednensday);
+thursday.addEventListener("click", openThursday);
+friday.addEventListener("click", openFriday);
+saturday.addEventListener("click", openSaturday);
 
 //Functions
 function addTodoItem(event) {
@@ -23,4 +48,40 @@ function addTodoItem(event) {
   } else {
     alert("add some text before adding your task");
   }
+  todoInput.value = "";
+}
+function checkForActiveDay() {
+  for (let i = 0; i < todoDaysCanvas.length; i++) {
+    if (todoDaysCanvas[i].classList.contains("active")) {
+      todoDaysCanvas[i].classList.remove("active");
+    }
+  }
+}
+function openSunday() {
+  checkForActiveDay();
+  day1.classList.add("active");
+}
+function openMonday() {
+  checkForActiveDay();
+  day2.classList.add("active");
+}
+function openTuesday() {
+  checkForActiveDay();
+  day3.classList.add("active");
+}
+function openWednensday() {
+  checkForActiveDay();
+  day4.classList.add("active");
+}
+function openThursday() {
+  checkForActiveDay();
+  day5.classList.add("active");
+}
+function openFriday() {
+  checkForActiveDay();
+  day6.classList.add("active");
+}
+function openSaturday() {
+  checkForActiveDay();
+  day7.classList.add("active");
 }
