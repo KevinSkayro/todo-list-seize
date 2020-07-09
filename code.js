de101a3a25d6bc9e4cf2efe0ebbc87a884cb9d07
Kevin Skayro <kev1n-1997@outlook.com>
2020-07-09 08:59:57 -0400

    added a little style to the task list, now the day updates on the upper bar, fixed css bug that made some content to be out of screen, working on task counter

--- code.js
+++ code.js
@@ -1,9 +1,12 @@
 //Global selections and variables
 const todoInput = document.querySelector(".todo-input");
 const todoInputBtn = document.querySelector(".btn-input");
-const todoCanvas = document.querySelector(".todo-list");
 const todoDays = document.querySelectorAll(".week");
 const todoDaysCanvas = document.querySelectorAll(".todo-canva");
+const daySelected = document.querySelector(".day-selected");
+const targetday = document.querySelectorAll(".target-day");
+const taskNum = document.querySelectorAll(".task-num");
+const todoItem = document.querySelectorAll(".todo-container");
 //days of the week selectors
 const sunday = document.querySelector(".sunday");
 const monday = document.querySelector(".monday");
@@ -38,11 +41,28 @@ function addTodoItem(event) {
   if (todoInput.value.length != 0) {
     const divItem = document.createElement("div");
     divItem.classList.add("todo-container");
+    //adding classes to identify different day's tasks
+    if (day1.classList.contains("active")) {
+      divItem.classList.add("d1");
+    } else if (day2.classList.contains("active")) {
+      divItem.classList.add("d2");
+    } else if (day3.classList.contains("active")) {
+      divItem.classList.add("d3");
+    } else if (day4.classList.contains("active")) {
+      divItem.classList.add("d4");
+    } else if (day5.classList.contains("active")) {
+      divItem.classList.add("d5");
+    } else if (day6.classList.contains("active")) {
+      divItem.classList.add("d6");
+    } else {
+      divItem.classList.add("d7");
+    }
 
     const newItem = document.createElement("li");
     newItem.innerText = todoInput.value;
     newItem.classList.add("todo-item");
-
+    // newItem.setAttribute("value", "1");
+    //this appends the input to the active day in the app
     divItem.appendChild(newItem);
     for (let i = 0; i < todoDaysCanvas.length; i++) {
       if (todoDaysCanvas[i].classList.contains("active")) {
@@ -50,10 +70,12 @@ function addTodoItem(event) {
       }
     }
   } else {
-    alert("add some text before adding your task");
+    alert("Add some text before adding your task");
   }
+  // updateTaskNum();
   todoInput.value = "";
 }
+//checks for which day is active, and checks when user clicks on diferent day and performs the change
 function checkForActiveDay() {
   for (let i = 0; i < todoDaysCanvas.length; i++) {
     if (todoDaysCanvas[i].classList.contains("active") && todoDays[i].classList.contains("active")) {
@@ -62,38 +84,60 @@ function checkForActiveDay() {
     }
   }
 }
+
+//focus on input when loading page
+function FocusOnInput() {
+  todoInput.focus();
+}
+
 function openSunday() {
   checkForActiveDay();
+  daySelected.innerText = "Sunday";
   day1.classList.add("active");
   sunday.classList.add("active");
 }
 function openMonday() {
   checkForActiveDay();
+  daySelected.innerText = "Monday";
   day2.classList.add("active");
   monday.classList.add("active");
 }
 function openTuesday() {
   checkForActiveDay();
+  daySelected.innerText = "Tuesday";
   day3.classList.add("active");
   tuesday.classList.add("active");
 }
 function openWednensday() {
   checkForActiveDay();
+  daySelected.innerText = "Wednesday";
   day4.classList.add("active");
   wednesday.classList.add("active");
 }
 function openThursday() {
   checkForActiveDay();
+  daySelected.innerText = "Thursday";
   day5.classList.add("active");
   thursday.classList.add("active");
 }
 function openFriday() {
   checkForActiveDay();
+  daySelected.innerText = "Friday";
   day6.classList.add("active");
   friday.classList.add("active");
 }
 function openSaturday() {
   checkForActiveDay();
+  daySelected.innerText = "Saturday";
   day7.classList.add("active");
   saturday.classList.add("active");
 }
+//Update the tasks quantity num on sidebar
+// function updateTaskNum() {
+//   for (let i = 0; i < todoDays.length; i++) {
+//     if (todoDays[i].classList.contains("active")) {
+//       taskNum[i].innerText = todoItem.length;
+//       console.dir(todoItem);
+//     }
+//   }
+// }
